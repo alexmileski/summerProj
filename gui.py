655649bc6a8c65c0cc2a24108e7df6a7d8bbeee8
alexmile <alexmile@umich.edu>
2023-07-15 19:40:24 -0400

    first simple gui

---
+++ gui.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg
+
+layout = [[sg.Text("Hello from PySimpleGUI")], [sg.Button("OK")]]
+
+# Create the window
+window = sg.Window("Demo", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == "OK" or event == sg.WIN_CLOSED:
+        break
+
+window.close()
+
+# must use xlaunch to see the GUI https://aaroalhainen.medium.com/working-with-guis-in-wsl2-790ed1653279
